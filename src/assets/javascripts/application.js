e3ee7885d7d65c04ea5618af74b1292e091c0a4b
Sergio Sisternes <sesispla@outlook.com>
2017-09-01 18:47:16 +0200

    Removed old reference to mydocs breaking the search functionality

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -191,7 +191,7 @@ function initialize(config) { // eslint-disable-line func-style
   new Material.Event.Listener("[data-md-component=query]", [
     "focus", "keyup", "change"
   ], new Material.Search.Result("[data-md-component=result]", () => {
-    return fetch(`${config.url.base}/mydocs/search_index.json`, {
+    return fetch(`${config.url.base}/docarys/search_index.json`, {
       credentials: "same-origin"
     }).then(response => response.json())
       .then(data => {
